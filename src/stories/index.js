35cc75679bcc546758e5a72234280be625b6c80a
Charlie Greenman <charie@razroo.com>
2017-08-13 11:00:21 -0400

    Add story for the show hide button.

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,9 +5,13 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
+import Form from "../components/showHide/showHide.js";
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('Show Hide', module)
+  .add('with text', () => <Form onClick={action('clicked')}>Hello Button</Form>)
