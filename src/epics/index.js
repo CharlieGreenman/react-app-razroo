43ce380cb2b6c771ef1d646bfc62a8792b4a6aeb
Charlie Greenman <charlie@razroo.com>
2017-08-24 13:07:25 -0400

    Benchmark progress for creating a root epic for the store.

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,4 +1,6 @@
 import { combineEpics } from 'redux-observable';
+import { showHide } from './showHideEpic';
 
 export const rootEpic = combineEpics(
+  showHide
 );
--- src/epics/showHideEpic.js
+++ src/epics/showHideEpic.js
@@ -1,7 +1,7 @@
 import * as ActionTypes from '../constants/ActionTypes';
 import { showBlock, hideBlock } from '../actions/showHide';
 
-export default function showHide(action$) {
+export function showHide(action$) {
   return action$.ofType(ActionTypes.SHOW)
     .map(action => showBlock)
 };
