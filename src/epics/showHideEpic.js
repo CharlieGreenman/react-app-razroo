7d975762321226b30ebb8f7bddb8dc6811f9c11e
Charlie Greenman <charie@razroo.com>
2017-08-29 22:20:59 -0400

    Benchmark progress for including timer in show hide functionality. Timer now set to change every second.

--- src/epics/showHideEpic.js
+++ src/epics/showHideEpic.js
@@ -1,16 +1,19 @@
 import * as ActionTypes from '../constants/ActionTypes';
 import { showBlock, hideBlock } from '../actions/showHide';
-import 'rxjs';
 import { Observable } from 'rxjs/Observable';
 
 export function showHide(action$) {
   return action$.ofType(ActionTypes.SHOW)
     .switchMap(action => {
       return Observable
-        .timer(0, 1000)
+        .timer(1000, 1000)
         .mergeMap(tick => {
           console.log('tick: ' + tick);
           return Observable.of(tick);
+        })
+        .map(seconds => {
+          console.log('seconds: ' + seconds);
+          return action;
         });
     });
 };
