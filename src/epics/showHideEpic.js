f40fef89f45ea38138908732340f96f150deb435
Charlie Greenman <charie@razroo.com>
2017-08-29 22:12:47 -0400

    Benchmark progress towards creating redux-observable timer to be used with showHide functionality.

--- src/epics/showHideEpic.js
+++ src/epics/showHideEpic.js
@@ -1,10 +1,16 @@
 import * as ActionTypes from '../constants/ActionTypes';
 import { showBlock, hideBlock } from '../actions/showHide';
+import 'rxjs';
+import { Observable } from 'rxjs/Observable';
 
 export function showHide(action$) {
   return action$.ofType(ActionTypes.SHOW)
     .switchMap(action => {
-      console.log('action: ' + action);
-      showBlock(action$)
+      return Observable
+        .timer(0, 1000)
+        .mergeMap(tick => {
+          console.log('tick: ' + tick);
+          return Observable.of(tick);
+        });
     });
 };
