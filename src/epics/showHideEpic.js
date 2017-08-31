2e84a24e956f401fbdbb4de5fe7855669a82e50c
Charlie Greenman <charie@razroo.com>
2017-08-30 21:13:16 -0400

    Create a showHideEpic to be used with showHide app.

--- src/epics/showHideEpic.js
+++ src/epics/showHideEpic.js
@@ -3,17 +3,12 @@ import { showBlock, hideBlock } from '../actions/showHide';
 import { Observable } from 'rxjs/Observable';
 
 export function showHide(action$) {
-  return action$.ofType(ActionTypes.SHOW)
-    .switchMap(action => {
-      return Observable
-        .timer(1000, 1000)
-        .mergeMap(tick => {
-          console.log('tick: ' + tick);
-          return Observable.of(tick);
-        })
-        .map(seconds => {
-          console.log('seconds: ' + seconds);
-          return action;
-        });
-    });
+
+  return action$.ofType(ActionTypes.SHOW).switchMap(action => {
+    return Observable
+      .timer(1000)
+      .map(seconds => {
+        return hideBlock();
+      })
+  });
 };
