491fb440494e33bff9ff5f4a3ddcd494279f4327
Charlie Greenman <charie@razroo.com>
2017-09-07 21:40:24 -0400

    Benchmark progress for creating unit tests for redux actions.

---
+++ src/actions/tests/unit/showHide.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import {showBlock, hideBlock} from '../../showHide.js';
