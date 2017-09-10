968c8a4bc2260b21ed11c64c885220fc4aa320a9
Charlie Greenman <charie@razroo.com>
2017-09-09 23:29:19 -0400

    Create a unit test for the environment reducer if showHide contains true.

--- src/reducers/tests/unit/environment.test.js
+++ src/reducers/tests/unit/environment.test.js
@@ -4,3 +4,10 @@ import environment from "../../environment";
 it('should show the initialState if action type is undefined', function () {
   expect(environment(undefined, {})).toEqual({show: false});
 });
+
+it('should set show to true if action type is show', function () {
+  expect(environment(undefined, {
+      type: types.SHOW,
+      show: true
+  })).toEqual({show: true});
+});
