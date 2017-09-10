81cfb1d6a9f490cda7b4b641e9091387accfbdf7
Charlie Greenman <charie@razroo.com>
2017-09-09 23:17:39 -0400

    Benchmark progress for creating unit tests for the environment reducer.

--- src/reducers/tests/unit/environment.test.js
+++ src/reducers/tests/unit/environment.test.js
@@ -1,5 +1,6 @@
 import * as types from "../../../constants/ActionTypes";
+import environment from "../../environment";
 
-it('should show the block if action type is show', function () {
-
+it('should show the initialState if action type is undefined', function () {
+  expect(environment(undefined, {})).toEqual({show: false});
 });
