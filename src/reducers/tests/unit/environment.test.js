e0950d0586df49783a87b75c60a3b34ed255b2f9
Charlie Greenman <charie@razroo.com>
2017-09-09 23:45:20 -0400

    Add unit tests for the environment reducer when attempting to hide block.

--- src/reducers/tests/unit/environment.test.js
+++ src/reducers/tests/unit/environment.test.js
@@ -5,6 +5,13 @@ it('should show the initialState if action type is undefined', function () {
   expect(environment(undefined, {})).toEqual({show: false});
 });
 
+it('should set show to false if action type is hide', function () {
+  expect(environment(undefined, {
+      type: types.HIDE,
+      show: true
+  })).toEqual({show: true});
+});
+
 it('should set show to true if action type is show', function () {
   expect(environment(undefined, {
       type: types.SHOW,
